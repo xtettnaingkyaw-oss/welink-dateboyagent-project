d1c1e801f15cad644c631a287e89c370ed126b7e
xtettnaingkyaw-oss <xtettnaingkyaw@gmail.com>
2026-09-15 15:07:10 +0630

    Update firebase.js

--- frontend-web/src/config/firebase.js
+++ frontend-web/src/config/firebase.js
@@ -1,1 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
+// သင့်ရဲ့ Firebase Project က Web Config များကို ဤနေရာတွင် အစားထိုးပါမည်
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+// Firebase ကို စတင်ခြင်း
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
