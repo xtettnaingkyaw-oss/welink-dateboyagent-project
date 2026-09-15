c4e52e4e3b6adac8a1fe53519f40ebfc09672078
xtettnaingkyaw-oss <xtettnaingkyaw@gmail.com>
2026-09-15 14:53:04 +0630

    Update firebase-config.js

--- backend-bot/firebase-config.js
+++ backend-bot/firebase-config.js
@@ -1,1 +1,10 @@
+const admin = require("firebase-admin");
+const serviceAccount = require("./serviceAccountKey.json");
 
+// Firebase Admin ကို Initialize လုပ်ခြင်း
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
+module.exports = { db };
